57a3d0215f0a878204d0c1d4c063607ea36d79ee
Omar <omar_morshedy@yahoo.com>
2022-04-27 17:14:57 +0100

    Added comments.

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
+# Upon importing a library that isn't included in the requirements file:
+# pip freeze > requirements.txt
+
+# Notes:
+#   - Use SQL alchemy or Pymssql utils.
+
 import pandas as pd
+
 print("Hello World")
 
 print(pd.__version__)
