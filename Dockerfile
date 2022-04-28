03b15dc99af8c17aac75bf221635cd214c785bae
Omar <omar_morshedy@yahoo.com>
2022-04-28 18:46:22 +0100

    added my sql stuff

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
-FROM python:3
+FROM ubuntu:20.04
+
+MAINTAINER Omar Ahmed
+
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN apt update && apt install -y python3 pip mysql-server && apt-get clean
+RUN pip install mysql-connector-python
 
 WORKDIR /usr/src/app
 
@@ -7,4 +14,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD [ "python", "./test.py" ]
+CMD [ "python3", "./test.py" ]
--- test.py
+++ test.py
@@ -4,8 +4,40 @@
 # Notes:
 #   - Use SQL alchemy or Pymssql utils.
 
+import os
+import json
+import pymysql
 import pandas as pd
+import fhiry.parallel as fp
 
-print("Hello World")
 
-print(pd.__version__)
+
+# do validation and checks before insert
+def validate_string(val):
+    if val is not None:
+        if type(val) is int:
+            # for x in val:
+            #   print(x)
+            return str(val).encode('utf-8')
+        else:
+            return val
+
+
+# read JSON file which is in the next parent folder
+# with open('data/Aaron697_Dickens475_8c95253e-8ee8-9ae8-6d40-021d702dc78e.json', 'r') as f:
+    # json_data = pd.read_json(f, orient='index')
+
+df = fp.process('data')
+print(df.head())
+
+# print(json_data)
+
+# connect to MySQL
+con = pymysql.connect(host='localhost', user='root', passwd='password', db='test')
+cursor = con.cursor()
+
+
+# Parse into SQL server
+# cursor.execute("INSERT INTO testp (person,	year,	company) VALUES (%s,	%s,	%s)", (person,	year,	company))
+# con.commit()
+# con.close()
